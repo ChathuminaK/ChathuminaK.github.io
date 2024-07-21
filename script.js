115b5a3e82b2572ae26d62e426b4787f2e5b0723
Restyled.io <commits@restyled.io>
2024-07-21 04:18:00 +0000

    Restyled by clang-format

--- script.js
+++ script.js
@@ -1,99 +1,84 @@
-//turn pages when click next or prev button
+// turn pages when click next or prev button
 const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
 pageTurnBtn.forEach((el, index) => {
-    el.onclick = () => {
+  el.onclick =
+      () => {
         const pageTurnId = el.getAttribute('data-page');
         const pageTurn = document.getElementById(pageTurnId);
 
         if (pageTurn.classList.contains('turn')) {
-            pageTurn.classList.remove('turn');
-            setTimeout(() => {
-                pageTurn.style.zIndex = 20 - index;
-            }, 500)
-        }else {
-            pageTurn.classList.add('turn');
-            setTimeout(() => {
-                pageTurn.style.zIndex = 20 + index;
-            }, 500)
+          pageTurn.classList.remove('turn');
+          setTimeout(() => { pageTurn.style.zIndex = 20 - index; }, 500)
+        } else {
+          pageTurn.classList.add('turn');
+          setTimeout(() => { pageTurn.style.zIndex = 20 + index; }, 500)
         }
-    }
+      }
 })
 
-//contact me button when click
+// contact me button when click
 const pages = document.querySelectorAll('.book-page.page-right');
 const contactMeBtn = document.querySelector('.btn.contact-me');
 
-contactMeBtn.onclick = () => {
-    pages.forEach((page, index) => {
-        setTimeout(() => {
-            page.classList.add('turn');
-            setTimeout(() => {
-                page.style.zIndex = 20 + index;
-            }, 500)  
-        }, (index + 1) * 200 + 100)
-    })
-}
+contactMeBtn.onclick =
+    () => {
+      pages.forEach((page, index) => {setTimeout(() => {
+                      page.classList.add('turn');
+                      setTimeout(() => { page.style.zIndex = 20 + index; }, 500)
+                    }, (index + 1) * 200 + 100)})
+    }
 
-
-//create reverse index function
+// create reverse index function
 let totalPages = pages.length;
 let pageNumber = 0;
 
 function reverseIndex() {
-    pageNumber--;
-    if(pageNumber < 0){
-        pageNumber = totalPages - 1;
-    }
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = totalPages - 1;
+  }
 }
 
-//back profile button when click
+// back profile button when click
 const backProfileBtn = document.querySelector('.back-profile');
 
-backProfileBtn.onclick = () => {
-    pages.forEach((_, index) => {
-        setTimeout(() => {
-            reverseIndex();
-            pages[pageNumber].classList.remove('turn');
+backProfileBtn.onclick =
+    () => {
+      pages.forEach((_, index) => {setTimeout(() => {
+                      reverseIndex();
+                      pages[pageNumber].classList.remove('turn');
 
-            setTimeout(() => {
-                reverseIndex();
-                pages[pageNumber].style.zIndex = 10 + index;
-            }, 500)
-        }, (index + 1) * 200 + 100)
-    })
-}
+                      setTimeout(() => {
+                        reverseIndex();
+                        pages[pageNumber].style.zIndex = 10 + index;
+                      }, 500)
+                    }, (index + 1) * 200 + 100)})
+    }
 
-
-//opening animation
+// opening animation
 const coverRight = document.querySelector('.cover.cover-right');
 const pageLeft = document.querySelector('.book-page.page-left');
 
-//opening animation (cover right animation)
-setTimeout(() => {
-    coverRight.classList.add('turn');
-}, 2100)
+// opening animation (cover right animation)
+setTimeout(() => { coverRight.classList.add('turn'); }, 2100)
 
-setTimeout(() => {
-    coverRight.style.zIndex = -1;
-}, 2800)
+setTimeout(() => { coverRight.style.zIndex = -1; }, 2800)
 
-//opening animation (page Left or profile page animation)
-setTimeout(() => {
-    pageLeft.style.zIndex = 20;
-}, 3200)
+// opening animation (page Left or profile page animation)
+setTimeout(() => { pageLeft.style.zIndex = 20; }, 3200)
 
-//opening animation (all page right animation)
-pages.forEach((_, index) => {
-    setTimeout(() => {
-        reverseIndex();
-        pages[pageNumber].classList.remove('turn');
+// opening animation (all page right animation)
+pages.forEach((_, index) => {setTimeout(
+                  () => {
+                    reverseIndex();
+                    pages[pageNumber].classList.remove('turn');
 
-        setTimeout(() => {
-            reverseIndex();
-            pages[pageNumber].style.zIndex = 10 + index;
-        }, 500)
-    }, (index + 1) * 200 + 2100)
-})
+                    setTimeout(() => {
+                      reverseIndex();
+                      pages[pageNumber].style.zIndex = 10 + index;
+                    }, 500)
+                  },
+                  (index + 1) * 200 + 2100)})
 
-//opening animation (page left or profile page animation)
+    // opening animation (page left or profile page animation)
